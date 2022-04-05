609594c3058278d57737a92972d36a2eb429e28f
Adrian <35572104+hypervisor@users.noreply.github.com>
2022-04-05 18:24:23 +0200

    handle invalid orgNr on client side + cleanup

--- node-api/public/javascripts/client.js
+++ node-api/public/javascripts/client.js
@@ -8,54 +8,62 @@ function runCheck() {
             return;
         }
 
+        // Remove all spaces
         input = input.replaceAll(' ', '');
 
         const url = `/api/v1/company/${input}`;
         console.log(`Checking company ${input}`);
         console.log(url);
 
-        document.getElementById('pep-response').innerHTML = "Searching...";
+        const resp = document.getElementById('pep-response');
+        resp.innerHTML = "Searching...";
 
-        fetch(url).then((res) => {
+        fetch(url).then(res => {
+            if (!res.ok) {
+                throw Error(res.statusText);
+            }
             return res.json();
-        }).then((data) => {
+        }).then(data => {
             const { matches } = data;
             if (matches.length == 0) {
-                document.getElementById('pep-response').innerHTML = "No PEPs found in company!";
+                resp.innerHTML = "No PEPs found in company!";
             } else {
                 let resp = '';
                 for (const el of matches) {
                     resp += `${el.name} (${el.rolletype})<br>`;
                 }
-                document.getElementById('pep-response').innerHTML = resp;
+                resp.innerHTML = resp;
             }
         }).catch((reason) => {
+            resp.innerHTML = "Invalid organization number!";
             console.error(`Exception in fetch callback: ${reason}`);
         });
+
     } else {
-        
+
         if (input.length == 0) {
             alert('Please enter a name');
             return;
         }
-    
+
         const caseSensitive = document.getElementById('pep-case').checked;
         const url = encodeURI(`/api/v1/person/${input}?caseSensitive=${caseSensitive}`);
-    
+
         console.log(`Checking person ${input}, caseSensitive: ${caseSensitive}`);
         console.log(url);
-    
-        document.getElementById('pep-response').innerHTML = "Searching...";
-    
-        fetch(url).then((res) => {
+
+        const resp = document.getElementById('pep-response');
+        resp.innerHTML = "Searching...";
+
+        fetch(url).then(res => {
             return res.json();
-        }).then((data) => {
+        }).then(data => {
             console.log(data);
             const { isPep } = data;
-            const resp = document.getElementById('pep-response');
-            resp.innerHTML = isPep + (isPep
-                ? ', person is politically exposed.'
-                : ', person is not known to be politically exposed.');
+
+            resp.innerHTML = isPep + (isPep ?
+                ', person is politically exposed.' :
+                ', person is not known to be politically exposed.');
         }).catch((reason) => {
             console.error(`Exception in fetch callback: ${reason}`);
         });
