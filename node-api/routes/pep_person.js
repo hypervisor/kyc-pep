4eb2c1b98a2647d675a0de3b30a43224559be9f4
Adrian <35572104+hypervisor@users.noreply.github.com>
2022-04-05 16:02:32 +0200

    WIP company checking

--- node-api/routes/pep_person.js
+++ node-api/routes/pep_person.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../private/db');
 const router = express.Router();
 
-/* GET users listing. */
+/* GET PEP-check for one person. */
 router.get('/:name', function(req, res, next) {
   try {
     const caseSensitive = (req.query.caseSensitive === 'true');
