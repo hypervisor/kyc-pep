f16f82024c20c3906df7d68c62fd05872215ba93
Adrian <35572104+hypervisor@users.noreply.github.com>
2022-04-05 18:19:13 +0200

    handle invalid orgNr on server side

--- node-api/routes/pepCompany.js
+++ node-api/routes/pepCompany.js
@@ -5,47 +5,56 @@ const router = express.Router();
 
 /* GET PEP-check for a company. */
 router.get('/:orgNr', function(req, res, next) {
-  try {
-    const orgNr = req.params.orgNr;
+    try {
+        const orgNr = req.params.orgNr;
 
-    const url = `https://code-challenge.stacc.dev/api/roller?orgNr=${orgNr}`;
-    console.log(url);
+        const url = `https://code-challenge.stacc.dev/api/roller?orgNr=${orgNr}`;
+        console.log(url);
 
-    fetch(url).then((res) => {
-        return res.json();
-    }).then((data) => {
-        // Array of employees in company that were found in the PEP database.
-        const pepMatches = [];
+        fetch(url).then(response => {
+            if (response.status != 200) {
+                res.sendStatus(response.status);
+                return;
+            }
+            return response.json();
+        }).then(data => {
+            // If invalid orgNr was given data will be null.
+            if (data == null) {
+                return;
+            }
+            console.log(data);
+            // Array of employees in company that were found in the PEP database.
+            const pepMatches = [];
 
-        const rollegruppetyper = data;
-        for (const rollegruppetype of rollegruppetyper) {
-            console.log(rollegruppetype.type.beskrivelse);
-            for (const rolletype of rollegruppetype.roller) {
-                //console.log(rolletype.person);
-                if (rolletype.person == null) {
-                    continue;
-                }
+            const rollegruppetyper = data;
+            for (const rollegruppetype of rollegruppetyper) {
+                console.log(rollegruppetype.type.beskrivelse);
+                for (const rolletype of rollegruppetype.roller) {
+                    //console.log(rolletype.person);
+                    if (rolletype.person == null) {
+                        continue;
+                    }
 
-                const fullName = `${rolletype.person.navn.fornavn} ${rolletype.person.navn.etternavn}`;
-                console.log('\t' + fullName);
+                    const fullName = `${rolletype.person.navn.fornavn} ${rolletype.person.navn.etternavn}`;
+                    console.log('\t' + fullName);
 
-                if (db.search(fullName, false)) {
-                    pepMatches.push({
-                        rolletype: rolletype.type.beskrivelse,
-                        name: fullName
-                    });
+                    if (db.search(fullName, false)) {
+                        pepMatches.push({
+                            rolletype: rolletype.type.beskrivelse,
+                            name: fullName
+                        });
+                    }
                 }
             }
-        }
 
-        res.json({ matches: pepMatches });
-    }).catch((reason) => {
-        console.error(`Exception in fetch callback: ${reason}`);
-    });
-  } catch(err) {
-    console.error(err.message);
-    res.sendStatus(500);
-  }
+            res.json({ matches: pepMatches });
+        }).catch((reason) => {
+            console.error(`Exception in fetch callback: ${reason}`);
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
