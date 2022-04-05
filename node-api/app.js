7eed9aa5f22eb6d18afc6f2ba0daf9298e259b9e
Adrian <35572104+hypervisor@users.noreply.github.com>
2022-04-06 00:41:37 +0200

    health check

--- node-api/app.js
+++ node-api/app.js
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/person/', pepPerson);
 app.use('/company/', pepCompany);
+app.get('/health', (req, res) => res.sendStatus(200));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
